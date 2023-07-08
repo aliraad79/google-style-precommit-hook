d1f1491eee25602340e660e8f6457b1d0c7183bf
Ali Ahmadi <kaf.ahmadi@tapsi.cab>
2023-07-08 12:12:13 +0330

    Add more logs

--- format-code.sh
+++ format-code.sh
@@ -2,7 +2,7 @@
 mkdir -p .cache
 cd .cache
 
-JAVA_FORMAT_JAR_NAME=google-java-format-1.7-all-deps.jar
+JAVA_FORMAT_JAR_NAME=google-java-format-1.17.0-all-deps.jar
 
 if [ ! -f $JAVA_FORMAT_JAR_NAME ]
 then
@@ -12,5 +12,7 @@ fi
 cd ..
 
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
+echo "Changed Files: "
 echo $changed_java_files
+echo "-------------"
 java -jar .cache/$JAVA_FORMAT_JAR_NAME --replace $changed_java_files
